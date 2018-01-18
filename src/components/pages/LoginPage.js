ac21ab1a08e2946d7d1fd698b193fcecd571246a
Yazed Jamal <muyaszed@gmail.com>
2018-01-18 13:17:12 +0000

    added router

---
+++ src/components/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const LoginPage = (props) => {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  )
+}
+
+export default LoginPage
