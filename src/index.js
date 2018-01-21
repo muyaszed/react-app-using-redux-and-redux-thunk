047816b4f9f51a7711d01739673b739bd5b3c56e
Yazed Jamal <muyaszed@gmail.com>
2018-01-21 13:14:46 +0000

    make async request to server and render server validation errors

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import rootReducer from './rootReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'));
 registerServiceWorker();
