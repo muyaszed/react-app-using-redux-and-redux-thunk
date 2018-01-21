b9fefc1c73ea8d611fca99f0f57c49fe24770d72
Yazed Jamal <muyaszed@gmail.com>
2018-01-21 09:26:32 +0000

    login form with client side validation

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-const HomePage = (props) => {
+const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import LoginForm from '../forms/LoginForm'
 
-const LoginPage = (props) => {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  )
+class LoginPage extends React.Component {
+
+  submit = (data) => {
+    console.log(data)
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Login</h1>
+        <LoginForm submit={this.submit}/>
+      </div>
+    )
+  }
 }
+
 
 export default LoginPage
